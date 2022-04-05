592c03fbee71bca91bad725d43c90717228ea2de
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-04-05 16:47:39 +0300

    Superhero app interface created

---
+++ app/src/main/java/com/kabe/android101/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kabe.android101
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addSuperHero(view : View){
+
+    }
+
+}
