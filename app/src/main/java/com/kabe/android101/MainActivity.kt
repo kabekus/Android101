f9ae5e1c9a2ae2f4552bf88b675371d39f64b05c
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-04-05 18:14:23 +0300

    Added superhero builder

--- app/src/main/java/com/kabe/android101/MainActivity.kt
+++ app/src/main/java/com/kabe/android101/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.kabe.android101
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addSuperHero(view : View){
+        val name =  nameText.text.toString()
+        val age = ageText.text.toString().toIntOrNull()
+        val job = jobText.text.toString()
+
+        if (age == null){
+            resultText.text = "Doğru Yaşı Giriniz !!"
+        }else{
+            val superHero = SuperHero(name,age,job)
+            resultText.text = "İsim: ${name} \nYaş: ${age} \nMeslek: ${job} "
+        }
 
     }
-
 }
---
+++ app/src/main/java/com/kabe/android101/SuperHero.kt
@@ -0,0 +1,5 @@
+package com.kabe.android101
+
+class SuperHero(var name: String, var age: Int, var job: String ) {
+
+}
